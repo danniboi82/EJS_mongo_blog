ca767f9ed62a5a47136fc954b4e9d6ca66946148
unknown <danoh82@gmail.com>
2018-03-06 19:48:47 -0800

    mostly done, .substring still not working

--- control/blog-routes.js
+++ control/blog-routes.js
@@ -41,10 +41,47 @@ app.get('/blogs/:id', (req, res)=>{
         }
     })
 })
+
 //EDIT ROUTE
+app.get('/blogs/:id/edit', (req, res)=>{
+    Blog.findById(req.params.id, (error, foundBlog)=>{
+        if(error){
+            console.log("ERROR", error);
+        } else {
+            console.log("ConsoleLOG from EDIT ROUTE", foundBlog.content)
+            //render page with previous content but editable
+            res.render("editBlog", {blog: foundBlog})
+        }
+    })
+})
 
 //UPDATE ROUTE
+app.put('/blogs/:id', (req, res)=>{
+    let id = req.params.id;
+    let title = req.body.title;
+    let content = req.body.content;
+    let image = req.body.image;
+    Blog.findByIdAndUpdate(id, {title: title, content: content, image: image}, (error, updatedBlog)=>{
+        if(error){
+            console.log("google it");
+            res.redirect('/blogs');
+        } else {
+            // res.redirect("/blogs/:id", {blog: updatedBlog})
+            res.redirect(`/blogs/${id}`)
+        }
+    })
+})
 
 //DELETE ROUTE
+app.delete('/blogs/:id', (req, res)=>{
+    let id = req.params.id;
+    Blog.findByIdAndRemove(id, (error, blogDelete)=>{
+        if(error){
+            console.log(error, "TRY AGAIN AND AGAIN");
+        } else {
+            res.redirect('/blogs');
+        }
+    })
+})
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ let express = require('express');
 let app = express();
 let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
+let methodOverride = require('method-override');
 mongoose.connect("mongodb://localhost/blogPost");
   
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.static("semantic"));
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 
